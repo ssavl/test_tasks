a96c3b5cf482ab1279417f8924665b4277d4c7a1
ssavl <get-moscow@yandex.ru>
2021-02-05 20:04:17 +0300

    added docker-compose file

---
+++ quiz_django/quiz_django/quiz_django/config.py
@@ -0,0 +1,2 @@
+SECRET_KEY = 'jj*je)-8f^b+3b+yr15#5-oc5o$63r$%=ao@!%=-iti8cuvz0h'
+DEBUG = True
--- quiz_django/quiz_django/quiz_django/settings.py
+++ quiz_django/quiz_django/quiz_django/settings.py
@@ -11,10 +11,11 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-from decouple import config
+# from config import SECRET_KEY
 
-SECRET_KEY = config('SECRET_KEY')
-DEBUG = config('DEBUG', cast=bool)
+SECRET_KEY = 'jj*je)-8f^b+3b+yr15#5-oc5o$63r$%=ao@!%=-iti8cuvz0h'
+
+DEBUG = True
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
